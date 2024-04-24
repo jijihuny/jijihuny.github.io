86964d5ec6fc08164efbc2ad53e6af60f222301a
JIJIHUNY <dlwlgns2792@gmail.com>
2024-04-24 15:13:21 +0900

    lint

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${siteMetadata.siteUrl}/sitemap.xml`
+    sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
   }
 }
