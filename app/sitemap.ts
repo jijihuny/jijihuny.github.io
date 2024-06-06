e4651839ddc2aeca227dab7846d767e51990761b
JIJIHUNY <dlwlgns2792@gmail.com>
2024-06-06 17:50:09 +0900

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,8 +12,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: post.lastmod || post.date,
     }))
 
-    //, 'projects', 'tags'
-  const routes = ['', 'blog'].map((route) => ({
+  const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
