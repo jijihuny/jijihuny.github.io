ce259d8b37ed11d020f4153f760ef1060812e2d4
JIJIHUNY <dlwlgns2792@gmail.com>
2024-08-22 18:20:04 +0900

    add option

--- components/MathBox.tsx
+++ components/MathBox.tsx
@@ -2,10 +2,15 @@ import { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
+  border: boolean
 }
 
-const MathBox = ({ children }: Props) => (
-  <div className="border border-solid border-black p-3 font-latin-roman text-base">{children}</div>
+const MathBox = ({ children, border = true }: Props) => (
+  <div
+    className={`${border ? 'border border-solid border-black' : ''} p-3 font-latin-roman text-base`}
+  >
+    {children}
+  </div>
 )
 
 export default MathBox
