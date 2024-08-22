f245a10ba7ace13ddf2f7ac15263a1bdc1316382
JIJIHUNY <dlwlgns2792@gmail.com>
2024-08-22 22:20:17 +0900

    project data

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            그동안 수행한 프로젝트들을 소개합니다.
           </p>
         </div>
         <div className="container py-12">
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,15 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: '어디갈래',
+    description: `여행 경로를 기록하는 것을 목표로 기획한 서비스 '어디갈래'`,
+    href: 'https://github.com/boostcampwm2023/iOS03-TravelShare',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '2024 인하 인공지능 챌린지',
+    description: `2024 인하 인공지능 챌린지에서 경제 관련 질의응답을 위한 LLM을
+    개발하고 대상을 수상하였습니다.`,
+    href: 'https://dacon.io/competitions/official/236291/overview/description',
   },
 ]
 
